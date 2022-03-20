8843d4c8800f864c652343fd5ea5c6a0e1c01e79
052hide <hidetoshi1990@gmail.com>
2022-03-21 01:08:11 +0900

    add style

--- src/components/examples/reactQuery/ReactQueryQuery/ReactQueryQuery.tsx
+++ src/components/examples/reactQuery/ReactQueryQuery/ReactQueryQuery.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx'
+
 import { usePets } from '@/hooks/reactQuery/pet'
 
 export const ReactQueryQuery = () => {
@@ -6,9 +8,19 @@ export const ReactQueryQuery = () => {
   return (
     <div>
       {data && (
-        <ul>
+        <ul
+          className={clsx(
+            'tw-w-full',
+            'tw-flex tw-flex-col tw-justify-center',
+            'tw-divide-y'
+          )}
+        >
           {data.data.items.map((p) => {
-            return <li key={p.id}>{p.name}</li>
+            return (
+              <li key={p.id} className={clsx('tw-px-4 tw-py-2')}>
+                {p.name}
+              </li>
+            )
           })}
         </ul>
       )}

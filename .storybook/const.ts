2cae1fd3e8a5fb3e33843159d94ec6c86980e109
Hidetoshi <hidetoshi1990@gmail.com>
2022-03-21 19:42:36 +0900

    setup storybook viewports (#30)

---
+++ .storybook/const.ts
@@ -0,0 +1,58 @@
+export const customViewports = {
+  smallMobile: {
+    name: 'Small Mobile',
+    styles: {
+      width: '360px',
+      height: '640px',
+    },
+  },
+  pixel6: {
+    name: 'Pixel 6',
+    styles: {
+      width: '360px',
+      height: '780px',
+    },
+  },
+  iPhone13Mini: {
+    name: 'iPhone 13 Mini',
+    styles: {
+      width: '375px',
+      height: '812px',
+    },
+  },
+  iPhone13Pro: {
+    name: 'iPhone 13 Pro',
+    styles: {
+      width: '390px',
+      height: '844px',
+    },
+  },
+  iPhone13ProMax: {
+    name: 'iPhone 13 Pro Max',
+    styles: {
+      width: '428px',
+      height: '926px',
+    },
+  },
+  iPad: {
+    name: 'iPad',
+    styles: {
+      width: '810px',
+      height: '1080px',
+    },
+  },
+  iPadPro13: {
+    name: 'iPad Pro 12.9',
+    styles: {
+      width: '1024px',
+      height: '1366px',
+    },
+  },
+  macBookPro14: {
+    name: 'MacBook Pro 14',
+    styles: {
+      width: '1169px',
+      height: '1800px',
+    },
+  },
+}
